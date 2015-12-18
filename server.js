f9dded14c087dbc48835af690ce9a8414f51ff7d
Owenby, Craig Michael <craig.owenby@gatech.edu>
2015-12-18 02:04:57 -0500

    Added server IP address as a command line argument and removed automatic external IP detection.

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 var http = require("http");
 var st = require("st");
-var external_ip = require("external-ip");
-
-var ipInst = external_ip();
 
 var port = 3000;
+var hostname = process.argv[2] | "localhost";
 
 try {
-    
-    // Getting the server's external IP address
-    ipInst(function (error, ip) {
-        if (error) {
-            throw error;
-        }
-           
-        http.createServer(
-           st( { path: __dirname, index: "index.html", cache: true} )).listen(port, ip);
-        console.log("Server started at " + ip + ":" + port);
-    });
+    http.createServer(st( { path: __dirname, index: "index.html", cache: true})).listen(port, hostname);
 } catch (err) {
     console.log(err.name + ": " + err.message);
 }
+
+console.log("Server started at " + hostname + ":" + port);
